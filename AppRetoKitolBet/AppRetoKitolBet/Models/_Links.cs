668f8c3bf559c4358887ffd5085a8000c2f3daaa
Jon Carrasco <jcarrascobar@gmail.com>
2018-12-13 12:17:07 +0100

    .

--- AppRetoKitolBet/AppRetoKitolBet/Models/_Links.cs
+++ AppRetoKitolBet/AppRetoKitolBet/Models/_Links.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace AppRetoKirolBet.Models
+namespace AppRetoKitolBet.Models
 {
     public class _Links
     {
-        public int Id { get; set; }
-        public AssigneeWP Assignee { get; set; }
-        public TypeWP Type { get; set; }
-        public StatusWP Status { get; set; }
-        public Priority Priority { get; set; }
+        public List<_Links> _links { get; set; }
+        public Assignee Assignee { get; set; }
     }
 }
