91ca493faf1b0c23a033c7fbceb314407a7af1f2
Pilar G <pilargonzalezcla@gmail.com>
2019-01-25 14:58:09 +0100

    calendario

---
+++ Ksoft Project/KSProject/Models/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KSProject.Models
+{
+    public class Persona
+    {
+        public int Id {get;set;}
+        public int IdOP { get; set; }
+        public double TotalHorasEstimadas { get; set; }//horas anuales que tiene que hacer segun contrato
+        public double TotalHorasTrabajadas { get; set; }//horas realizadas
+        public List<Oporrak> Oporrak { get; set; }
+        public List<Dedication> Dedications { get; set; }
+    }
+}
